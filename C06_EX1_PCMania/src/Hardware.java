87d05b733b1eb003d5fb62ad05655f4c69d8d1b5
Gabriel Baldoni <158329569+baldonigabriel@users.noreply.github.com>
2025-04-02 23:02:08 -0300

    Delete C06_EX1_PCMania directory

--- C06_EX1_PCMania/src/Hardware.java
+++
@@ -1,9 +0,0 @@
-public class Hardware {
-    String tipo;   // Tipo de dispositivo (ex: Processador Core i5)
-    int capacidade; // Capacidade do dispositivo (ex: 4GB, 1200 MHz)
-
-    public Hardware(String tipo, int capacidade) {
-        this.tipo = tipo;
-        this.capacidade = capacidade;
-    }
-}
