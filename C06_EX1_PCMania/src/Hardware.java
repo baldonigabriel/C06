2f76c72c9acf77349fd834aad7cc835aac8b890c
Gabriel Baldoni <158329569+baldonigabriel@users.noreply.github.com>
2025-04-02 23:02:36 -0300

    Add files via upload

---
+++ C06_EX1_PCMania/src/Hardware.java
@@ -0,0 +1,9 @@
+public class Hardware {
+    String tipo;   // Tipo de dispositivo (ex: Processador Core i5)
+    int capacidade; // Capacidade do dispositivo (ex: 4GB, 1200 MHz)
+
+    public Hardware(String tipo, int capacidade) {
+        this.tipo = tipo;
+        this.capacidade = capacidade;
+    }
+}
